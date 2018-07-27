6651c617f151f3e2d464a748b4c93abd35b3f33d
Tom Hargreaves <hex@sphere.chronosempire.org.uk>
2018-07-27 17:16:57 +0100

    pucrunch spends a lot of time decompressing the brain picture. help it out a bit by not asking it to decompress so many repeated bytes

--- compress_assets.bat
+++ compress_assets.bat
@@ -1,3 +1,4 @@
+#dd if=data/flash-mode2.bin bs=256 skip=13 count=54 of=data/flash-mode2.smaller.bin
 bin\pucrunch.exe -d -c0 -l0x1000 data\dither.bin data\dither.pu
 bin\pucrunch.exe -d -c0 -l0x1000 data\hdither.bin data\hdither.pu
 bin\pucrunch.exe -d -c0 -l0x1000 data\hdither2.bin data\hdither2.pu
@@ -13,7 +14,7 @@ bin\pucrunch.exe -d -c0 -l0x1000 data\logo-only-mode2.bin data\twisted-logo.pu
 bin\pucrunch.exe -d -c0 -l0x1000 data\brain-only-mode2.bin data\brain-only.pu
 bin\pucrunch.exe -d -c0 -l0x1000 data\twisted-brain-mode2.bin data\twisted-brain.pu
 bin\pucrunch.exe -d -c0 -l0x1000 data\smiley-mode2.bin data\smiley.pu
-bin\pucrunch.exe -d -c0 -l0x1000 data\flash-mode2.bin data\brain-flash.pu
+bin\pucrunch.exe -d -c0 -l0x1000 data\flash-mode2.smaller.bin data\brain-flash.pu
 bin\pucrunch.exe -d -c0 -l0x1000 data\brain-mask-mode2.bin data\brain-mask.pu
 
-rem bin\pucrunch.exe -d -c0 -l0x1000 data\font_XXX.bin data\font_XXX.pu
+bin\pucrunch.exe -d -c0 -l0x1000 data\font_razor.bin data\font_razor.pu
